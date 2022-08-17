3a3c4da010045c47f2a3a8edec61146dfd0fb90d
zhukova_oksana <ksy2905@gmail.com>
2022-08-17 16:26:43 +0300

    durdom

--- js/Task03.js
+++ js/Task03.js
@@ -2,37 +2,46 @@
 
 
 
-// const checkRandomNumber2 = (item, n, m, str) => {
-//   const arr = [];
-//   while (arr.length !== item) {
-//     let x = Math.floor(Math.random() * (Math.max(n, m) - Math.min(n, m) + 1)) + Math.min(n, m);
-//     if ((str === 'even') && (x % 2 === 0 )) {
-//       arr.push(x);
-//     }
-//     if ((str === 'odd') && (x % 2 !== 0 )) {
-//       arr.push(x);
-//     }
-//   }
-//   return arr;
+const checkRandomNumber2 = (item, n, m, str) => {
+  const arr = [];
+  while (arr.length !== item) {
+    let x = Math.floor(Math.random() * (Math.max(n, m) - Math.min(n, m) + 1)) + Math.min(n, m);
+    if ((str === 'even') && (x % 2 === 0 )) {
+      arr.push(x);
+    }
+    if ((str === 'odd') && (x % 2 !== 0 )) {
+      arr.push(x);
+    }
+  }
+  return arr;
+}
+
+// const arrNumber = [10, 12, 34, 25, 67, 89, 80, 54, 65, 76, 100];
+//
+// const checkEven = (arr) => {
+//   return arr.filter((item) => item % 2 === 0);
 // }
+//
+// const checkOdd = (arr) => {
+//   return arr.filter((item) => item % 2 !== 0);
+// }
+//
+// console.log(checkEven(arrNumber));
+// console.log(checkOdd(arrNumber));
 
 const checkRandomNumber = (item, n, m, str) => {
-  let min = Math.min(n, m);
-  let max = Math.max(n, m);
-
-  let maxMin = max - min;
 
   if (str === 'even') {
-    return Array(item).fill().map(() => Math.floor(Math.random() * (maxMin / 2)) * 2);
+    return Array(item).fill().map(() => Math.floor(Math.random() * (Math.max(n, m) - Math.min(n, m) + 1)) + Math.min(n, m)).filter((num) => num % 2 ===0);
   }
 
   if (str === 'odd') {
-    return Array(item).fill().map(() => Math.floor(Math.random() * (maxMin / 2)) * 2 + 1);
+    return Array(item).fill().map(() => Math.floor(Math.random() * (Math.max(n, m) - Math.min(n, m) + 1)) + Math.min(n, m)).filter((num) => num % 2 !==0);
   }
 }
 
-// console.log('even', checkRandomNumber2(10, 1, -5, 'even'));
-// console.log('odd', checkRandomNumber2(10, -1, 9, 'odd'));
+console.log('while even', checkRandomNumber2(10, 1, -5, 'even'));
+console.log('while odd', checkRandomNumber2(10, -1, 9, 'odd'));
 
 console.log('even', checkRandomNumber(10, 1, -7, 'even'));
 console.log('odd', checkRandomNumber(10, 20, 9, 'odd'));
